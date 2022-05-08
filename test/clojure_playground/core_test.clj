d937b69f10b6270d49f8ed73c88cbf28518cb273
Julio Betta <juliobetta@gmail.com>
2022-05-08 01:45:50 -0300

    improve test case with multiple assertions

--- test/clojure_playground/core_test.clj
+++ test/clojure_playground/core_test.clj
@@ -15,7 +15,9 @@
 
 (deftest check-card-flag-test
   (testing "Check card flag"
-    (is (= (check-card-flag 12348790) "Master Card"))
-    (is (= (check-card-flag 34599994) "Visa"))
-    (is (= (check-card-flag 567991293) "AMEX"))
-    (is (= (check-card-flag 96986986) "Invalid number"))))
+    (are [number flag] (= (check-card-flag number) flag)
+      12348790 "Master Card"
+      34599994 "Visa"
+      567991293 "AMEX"
+      96986986 "Invalid number"
+    )))
