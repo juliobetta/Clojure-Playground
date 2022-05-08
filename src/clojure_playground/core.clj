3be5b8d2a92badf3c5b98c6b90e79158b6d3111c
Julio Betta <juliobetta@gmail.com>
2022-05-08 01:30:25 -0300

    first experiments

--- src/clojure_playground/core.clj
+++ src/clojure_playground/core.clj
@@ -1,6 +1,22 @@
-(ns clojure-playground.core)
+(ns clojure-playground.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; an example of multi-arity function
+(defn greetings
+  "Say hi!"
+  ([]       (greetings "Hello"))
+  ([prefix] (str prefix " there!")))
+
+; an example of multi param function
+(defn values [first & rest]
+  (str/join ", " (conj rest first)))
+
+; conditional example
+(defn check-card-flag
+  [first-numbers]
+  (cond
+    (str/starts-with? first-numbers "123") "Master Card"
+    (str/starts-with? first-numbers "345") "Visa"
+    (str/starts-with? first-numbers "567") "AMEX"
+    :else "Invalid number"
+  ))
--- test/clojure_playground/core_test.clj
+++ test/clojure_playground/core_test.clj
@@ -2,6 +2,20 @@
   (:require [clojure.test :refer :all]
             [clojure-playground.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest greetings-test
+  (testing "Greetings with no param"
+    (is (= (greetings) "Hello there!")))
+
+  (testing "Greetings with no param"
+    (is (= (greetings "Hi") "Hi there!"))))
+
+(deftest values-test
+  (testing "All Values function"
+    (is (= (values 1 2 3 4) "1, 2, 3, 4" ))))
+
+(deftest check-card-flag-test
+  (testing "Check card flag"
+    (is (= (check-card-flag 12348790) "Master Card"))
+    (is (= (check-card-flag 34599994) "Visa"))
+    (is (= (check-card-flag 567991293) "AMEX"))
+    (is (= (check-card-flag 96986986) "Invalid number"))))
